35978e58410d561e26588f19e703792ae9710ec7
cunweizhao <18500103508@163.com>
2020-12-24 12:03:52 +0800

    1.1
    可视化项目初始化

---
+++ zcw-datav-demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+/*全局的宽高设置*/
+  html,body,#app{
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
