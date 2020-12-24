ea49213816fa44bb462bd4b04bb5939fdbd21488
cunweizhao <18500103508@163.com>
2020-12-24 14:12:25 +0800

    1.1
    容器组件初始化

--- zcw-datav-demo/src/views/Home.vue
+++ zcw-datav-demo/src/views/Home.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="home">
-  report
+    <!--开始引用组件-->
+    <top-view/>
+    <bottom-view/>
+    <map-view/>
+    <sales-view/>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import TopView from './../components/TopView'
+import BottomView from './../components/BottomView'
+import MapView from './../components/MapView'
+import SalesView from './../components/SalesView'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  comments: {
+    TopView,
+    BottomView,
+    MapView,
+    SalesView
+  }
 }
 </script>
+<style>
+  .home{
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+  }
+</style>
