49d2933f36d65735894f1d96e29d2e8b75b04191
cunweizhao <18500103508@163.com>
2020-12-27 09:24:46 +0800

    1.0
    柱状图编写

--- zcw-datav-demo/src/components/SalesView/index.vue
+++ zcw-datav-demo/src/components/SalesView/index.vue
@@ -1,13 +1,32 @@
 <template>
-  <div> sales view</div>
+    <v-chart :options="data"/>
 </template>
 
 <script>
 export default {
-  name: 'SalesView'
+  name: 'SalesView',
+  data () {
+    return {
+      data: {
+        xAxis: {
+          typ: 'category'
+        },
+        yAxis: {
+
+        },
+        series: [{
+          type: 'line',
+          data: [100, 200, 300]
+        }]
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .echarts{
+    width: 100%;
+    height: 100%;
+  }
 </style>
